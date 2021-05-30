13e18e35ed6410f49e81dfb52e76912dff5c2ea6
Abideen Kareem <abideenkareem17@gmail.com>
2021-05-30 15:39:34 -0600

    edit intro statement

--- src/Introduction.js
+++ src/Introduction.js
@@ -15,7 +15,7 @@ export default function Introduction() {
                         My name is Abideen (Kos) Kareem and I'm a 
                         Software Engineer that utilizes React,
                         Node.js, JavaScript and more to develop
-                        immersive and interactive projects.
+                        intuitive and interactive projects.
                     </h1>
                 </div>
                 <span className="dv-intro-span" onClick={() => window.scrollTo({top: 1870, behavior: 'smooth'})}>Let Connect!</span>
