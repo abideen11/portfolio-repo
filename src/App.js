f7c13625ccbc8f4fb5785fdfef55121c8e5e0337
Abideen Kareem <abideenkareem17@gmail.com>
2021-09-08 13:52:59 -0600

    Removed commented lines

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import React from 'react'
-// import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Footer from './Footer';
 
 import ReactGA from 'react-ga';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 const TRACKING_ID = "G-KKFWDF1PY1";
 ReactGA.initialize(TRACKING_ID);
